94652f009062af24fab2d4498b1281c08f456e3f
SS <shreyansh1999@gmail.com>
2020-06-16 20:11:34 +0530

    Added sideworks file

---
+++ sideworks_file.py
@@ -0,0 +1,21 @@
+import os
+import wget
+import praw
+
+def download_image(url):
+    print("Attempting to download image")
+    try:
+        filename = wget.download(url)
+        return filename
+    except Exception as e:
+        print("Error in downloading image")
+        print(e)
+
+def remove_image(filename):
+    print("Attempting to remove image")
+    try:
+        if (filename is not None):
+            os.remove(filename)
+    except Exception as e:
+        print("Error in deleting file")
+        print(e)
